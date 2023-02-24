c9ccf00d32c945231ed10c9b9990f8a0fc0da3f3
Abhishek <abhi.jaiswal1494@gmail.com>
2023-02-24 18:43:28 +0530

    Node Mailer

---
+++ QR_BOT/backend_auth/index.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const app=express();
+ 
+const {passport}=require("passport");
+const {connection}=require("./configuration/db");
+const {gitRoute}=require("./auth/github")
+require('dotenv').config();
+const cors=require("cors");
+const {UserRouter}=require("./router/user");
+const {googleRouter}=require("./auth/google");
+const {fbRouter}=require("./auth/facebook")
+const {Qr_save_Router}=require("./router/qr_gen_save");
+const {adminRoute}=require("./router/admin.route");
+
+app.use(express.json());
+app.use(cors());
+
+app.use(adminRoute)
+app.use(UserRouter);
+app.use(googleRouter);
+app.use(gitRoute);
+app.use(Qr_save_Router);
+// app.use(fbRouter);
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("db is connected");
+    } catch (error) {
+        console.log("db is not connected");
+    }
+    console.log(`http://localhost:${process.env.port}/`);
+});
