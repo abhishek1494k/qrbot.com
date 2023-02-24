f7f39bfe1b6e9cbed42847e92dbeb243096765e5
Abhishek <abhi.jaiswal1494@gmail.com>
2023-02-24 18:08:02 +0530

    Node Mailer

--- MOhd_Yunus/backend_auth/router/admin.route.js
+++ MOhd_Yunus/backend_auth/router/admin.route.js
@@ -4,9 +4,6 @@ const {QRModel}=require("../model/qr.model");
 
 adminRoute.get("/user/detail",async(req,res)=>{
     try {
-        // console.log("object");
-        // let newdata=await UserModel.find();
-        // console.log(newdata[0]._id);
         let data= await UserModel.aggregate([{
             $lookup:
               {
