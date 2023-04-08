3de40db819415d8226043c8f49f29d7242e0bd8f
Abhishek <abhi.jaiswal1494@gmail.com>
2023-04-08 18:36:42 +0530

    Favicon added, Sigup/login code re-checked, nodemailer re-checked, spal added in login signup

--- QR_BOT/Backend/index.js
+++ QR_BOT/Backend/index.js
@@ -1,27 +1,30 @@
 const express=require("express");
 const app=express();
+
 require('dotenv').config();
 const cors=require("cors");
-const {passport}=require("passport");
+const passport=require("passport");
 const cookieParser = require('cookie-parser')
+
 app.use(express.json());
 app.use(cors());
 
-const {gitRoute}=require("./auth/github")
-const {connection}=require("./configuration/db");
+// const {gitRoute}=require("./auth/github")
+
 const {UserRouter}=require("./router/user");
-const {googleRouter}=require("./auth/google");
-const {fbRouter}=require("./auth/facebook");
-const {Qr_save_Router}=require("./router/qr_gen_save");
-const {adminRoute}=require("./router/admin.route");
+// const {googleRouter}=require("./auth/google");
+// const {fbRouter}=require("./auth/facebook");
+// const {Qr_save_Router}=require("./router/qr_gen_save");
+// const {adminRoute}=require("./router/admin.route");
 // app.use(gitRoute);
 
 app.use('/',UserRouter);
-app.use(googleRouter);
-app.use('/qr',Qr_save_Router);
-app.use('/user',adminRoute)
+// app.use(googleRouter);
+// app.use('/qr',Qr_save_Router);
+// app.use('/user',adminRoute)
 
 // ---------->>>>> Connection <<<<<------------//
+const {connection}=require("./configuration/db");
 app.listen(process.env.port,async()=>{
     try {
         await connection;
--- QR_BOT/Backend/router/user.js
+++ QR_BOT/Backend/router/user.js
@@ -1,108 +1,71 @@
 const express = require("express");
 const UserRouter = express.Router();
+
 const { UserModel } = require("../model/user_model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { Router } = require("express");
-const fs = require("fs");
-const nodemailer = require("nodemailer");
-const {BlacklistuserModel}=require("../model/blockusermodel");
-
-
-UserRouter.get('/',(req,res)=>{
-  res.send('Home')
-})
-
+const { BlacklistuserModel } = require("../model/blockusermodel");
 const { authentication } = require("../middleware/authenticate");
 
-UserRouter.get("/refresh", async (req, res) => {
-  let token = req.headers.authorization;
-  if (token) {
-    let decoded = jwt.verify(token, process.env.Refresh_token_secret);
-    if (decoded) {
-      console.log(decoded);
-      let dataid = decoded.dataid;
-      let new_token = jwt.sign({ dataid }, process.env.token_secret, {
-        expiresIn: 60,
-      });
-      res.send({ msg: "referesh token generrated", new_token });
-    }
-  } else {
-    res.send({ msg: "login again" });
-  }
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+
+// ------------->>>>> Home Page <<<<<----------------
+UserRouter.get("/", (req, res) => {
+  res.send("Home");
 });
 
-UserRouter.post("/logout", async (req, res) => {
-  try {
-    let token = req.headers.authorization;
-   
-    let blacklistAcc = JSON.parse(fs.readFileSync("./blacklist.json", "utf-8"));
-    console.log(blacklistAcc);
-    blacklistAcc.push(token);
-    console.log(blacklistAcc);
-    fs.writeFileSync("./blacklist.json", JSON.stringify(blacklistAcc));
-    res.send({ msg: "logout successfull" });
-  } catch (error) {
-    console.log(error);
-    res.send({ msg: "something went wrong" });
-  }
-});
-
-
-
+// --------------->>>>> Signup <<<<<-----------------
 UserRouter.post("/signup", async (req, res) => {
   try {
+    // Find in Blocked Emails
     const { name, email, password } = req.body;
-    let blackuser=await BlacklistuserModel.find({block_email:email})
-    if(blackuser){
-      return res.send({msg:'You have been Blocked'})
-    };
-    // let blockuser=JSON.parse(fs.readFileSync("./blacklistuser.json","utf-8"));
-    // if(blockuser.includes(email)){
-    //   return res.send({msg:'You have been Blocked'})
-    // }
-    const signedata = await UserModel.find({ email });
-    if (signedata.length == 0) {
+    let blockuser = await BlacklistuserModel.find({ block_email: email });
+    if (blockuser) {
+      return res.send({ msg: "You have been Blocked" });
+    }
+    // Find in User Database
+    const user = await UserModel.find({ email });
+    if (user.length === 0) {
       bcrypt.hash(password, 5, async (err, hash) => {
         if (!err) {
-          let data = { name, email, password: hash };
-          const UserData = new UserModel(data);
+          const UserData = new UserModel({ name, email, password: hash });
           await UserData.save();
-          res.send({ msg: "Successfully Signed Up" });
+          return res.send({ msg: "Successfully Signed Up" });
         } else {
-          console.log(err);
-          // res.send(err)
-          res.send({ error: err });
+          return res.send({ msg: "Error in Signup", error: err });
         }
       });
     } else {
-      res.send({ msg: "You have been already Signed Up" });
+      return res.send({ msg: "You are already Registered" });
     }
   } catch (error) {
-    console.log(error);
-    console.log("something went wrong");
-    res.send({ error: error });
+    res.send({ msg: "something went wrong", error: error });
   }
 });
 
+// ------------->>>>> Login User <<<<<----------------
 UserRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
   {
     try {
-      const { email, password } = req.body;
-      console.log("object");
-      let blackuser=await BlacklistuserModel.find({block_email:email})
-      if(blackuser){
-        return res.send({msg:'You have been Blocked'})
-      };
-      // let blockuser=JSON.parse(fs.readFileSync("./blacklistuser.json","utf-8"));
-      // if(blockuser.includes(email)){
-      //   return res.send({msg:'You have been Blocked'})
-      // }
+      // Find in Blocked Email
+      let blockuser = await BlacklistuserModel.find({ block_email: email });
+      console.log(blockuser)
+      if (blockuser.length >0) {
+        return res.send({ msg: "You have been Blocked" });
+      }
       let passdata = await UserModel.find({ email });
-      if (passdata.length == 1) {
+      if (passdata.length === 1) {
         bcrypt.compare(password, passdata[0].password, function (err, result) {
           if (result) {
-            console.log(passdata[0]._id);
             var token = jwt.sign(
               { dataid: passdata[0]._id, email: passdata[0].email },
               process.env.token_secret,
@@ -116,44 +79,48 @@ UserRouter.post("/login", async (req, res) => {
             // NODE MAILER-------------------------------------------------------------------------
             const transporter = nodemailer.createTransport({
               service: "gmail",
-              secure: false,
               auth: {
-                user: "abhi.jaiswal1494@gmail.com",
-                pass: "catttouunbkahzcr",
-              },
-              tls: {
-                rejectUnauthorized: false,
+                user: "forsmmpanel@gmail.com",
+                pass: "noymjrhbxjwiclin",
               },
             });
+
             const mailOptions = {
-              from: "abhi.jaiswal1494@gmail.com",
-              to: `${email}`,
-              subject: "LOGIN Successfull",
-              text: `You are now Logged in to QR Bot `,
+              from: "forsmmpanel@gmail.com",
+              to: email,
+              subject: "QRBot Login",
+              text: `You Loggedin into QRBot.com @ ${
+                year +
+                "-" +
+                month +
+                "-" +
+                date +
+                " " +
+                hours +
+                ":" +
+                minutes +
+                ":" +
+                seconds
+              } using Email : ${email}`,
             };
-            transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                console.log(error);
-                return res.status(500).json({ message: "Error Sending Mail" });
-              } else {
-                console.log("mail send");
-                // return res.status(200).json({ message: "OTP sent to email" });
-                res.send({
-                  msg: "Successfully logged in",
-                  token,
-                  Refreshtoken,
-                  name: passdata[0].name,
-                });
-              }
-            });
+
+            transporter
+              .sendMail(mailOptions)
+              .then((info) => {
+                console.log(passdata[0].name, token, Refreshtoken)
+                console.log('Mail sent')
+                res.send({ msg: "Login Successful",name:passdata[0].name, token:token, Refreshtoken:Refreshtoken });
+              })
+              .catch((e) => {
+                res.send(e);
+              });
             // -------------------------------------------------------------------------------------
           } else {
-            console.log(err);
             res.send({ msg: "Wrong Password" });
           }
         });
       } else {
-        res.send({ msg: "You have not been SignedUp yet" });
+        res.send({ msg: "You have not been Registered" });
       }
     } catch (error) {
       console.log(error);
@@ -163,6 +130,38 @@ UserRouter.post("/login", async (req, res) => {
   }
 });
 
+// UserRouter.post("/logout", async (req, res) => {
+//   try {
+//     let token = req.headers.authorization;
+
+//     let blacklistAcc = JSON.parse(fs.readFileSync("./blacklist.json", "utf-8"));
+//     console.log(blacklistAcc);
+//     blacklistAcc.push(token);
+//     console.log(blacklistAcc);
+//     fs.writeFileSync("./blacklist.json", JSON.stringify(blacklistAcc));
+//     res.send({ msg: "logout successfull" });
+//   } catch (error) {
+//     console.log(error);
+//     res.send({ msg: "something went wrong" });
+//   }
+// });
+
+// UserRouter.get("/refresh", async (req, res) => {
+//   let token = req.headers.authorization;
+//   if (token) {
+//     let decoded = jwt.verify(token, process.env.Refresh_token_secret);
+//     if (decoded) {
+//       console.log(decoded);
+//       let dataid = decoded.dataid;
+//       let new_token = jwt.sign({ dataid }, process.env.token_secret, {
+//         expiresIn: 60,
+//       });
+//       res.send({ msg: "referesh token generrated", new_token });
+//     }
+//   } else {
+//     res.send({ msg: "login again" });
+//   }
+// });
 
 module.exports = {
   UserRouter,
